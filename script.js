c70855ca011165550e81d37a69a39e6c3071d54e
daffanu23 <daffanugraha058@gmail.com>
2025-06-25 13:17:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
   import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-  const createClient = (
+const supabase = createClient(
     "https://kwjjrtfukjybwqwdequl.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3ampydGZ1a2p5Yndxd2RlcXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MzAyMjQsImV4cCI6MjA2NjQwNjIyNH0.iAYjFovdfeG7P6lwHwNOlO3Apww1uoQ8jxzu3mnT8d8"
   );
