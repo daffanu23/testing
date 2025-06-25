ddfaba69398f076297d6b9e3d467f337f84b3d08
daffanu23 <daffanugraha058@gmail.com>
2025-06-25 13:13:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+  const createClient = (
+    "https://kwjjrtfukjybwqwdequl.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3ampydGZ1a2p5Yndxd2RlcXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MzAyMjQsImV4cCI6MjA2NjQwNjIyNH0.iAYjFovdfeG7P6lwHwNOlO3Apww1uoQ8jxzu3mnT8d8"
+  );
+
+  async function simpan() {
+    const name = document.getElementById("name").value;
+    const {data, error} = await supabase.from("testing").insert([{name}])
+
+    if(error) return alert("Data gagal ditambahkan")
+        alert("Data berhasil di tambahkan")
+  }
+
+  window.simpan = simpan
